afd32e7bc68601b10301b78f063d9596bc9add71
farihazultana <farihasultana237@gmail.com>
2023-11-21 13:21:55 +0600

    TextInputEditText has been added

--- app/src/main/java/com/example/radiotoday/ui/activities/loginActivity.kt
+++ app/src/main/java/com/example/radiotoday/ui/activities/loginActivity.kt
@@ -1,11 +1,12 @@
-package com.example.radiotoday
+package com.example.radiotoday.ui.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.radiotoday.R
 
-class MainActivity : AppCompatActivity() {
+class loginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
     }
 }
