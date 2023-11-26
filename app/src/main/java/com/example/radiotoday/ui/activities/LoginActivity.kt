f1638f51e3ada70e2a4e00881e664bacf8bc9af8
farihazultana <farihasultana237@gmail.com>
2023-11-26 12:30:32 +0600

    Forget password dialog added

--- app/src/main/java/com/example/radiotoday/ui/activities/LoginActivity.kt
+++ app/src/main/java/com/example/radiotoday/ui/activities/LoginActivity.kt
@@ -1,13 +1,16 @@
 package com.example.radiotoday.ui.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewGroup
 import com.example.radiotoday.R
 import com.example.radiotoday.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
+    private lateinit var dialog: Dialog
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -21,6 +24,24 @@ class LoginActivity : AppCompatActivity() {
             binding.layoutLogin.visibility = View.VISIBLE
             binding.layoutRegistration.visibility = View.GONE
         }
+
+
+        binding.tvForget.setOnClickListener {
+            setDialog()
+            dialog.show()
+        }
+
+    }
+
+    private fun setDialog() {
+        dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_forget_password)
+        dialog.window?.setLayout(
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
+        dialog.setCancelable(true)
+        dialog.window!!.attributes!!.windowAnimations = R.style.animation
 
     }
 }
