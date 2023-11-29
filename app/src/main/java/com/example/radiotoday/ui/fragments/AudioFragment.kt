fd68c987b6e9806f03bf27fd285dea4b27d1b372
farihazultana <farihasultana237@gmail.com>
2023-11-29 15:55:52 +0600

    playList child item and news child item has been added

--- app/src/main/java/com/example/radiotoday/ui/fragments/AudioFragment.kt
+++ app/src/main/java/com/example/radiotoday/ui/fragments/AudioFragment.kt
@@ -3,31 +3,18 @@ package com.example.radiotoday.ui.fragments
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import com.example.radiotoday.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [AudioFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AudioFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
@@ -38,23 +25,21 @@ class AudioFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_audio, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment AudioFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            AudioFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.playitem_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
     }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+
+            R.id.item_share ->{
+                return true
+            }
+            R.id.item_download ->{
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
