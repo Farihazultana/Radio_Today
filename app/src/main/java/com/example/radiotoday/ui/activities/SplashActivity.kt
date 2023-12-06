d01b1f0247d97db260ea3992477b3aa6b7034cbc
farihazultana <farihasultana237@gmail.com>
2023-12-06 16:29:42 +0600

    Splash screen done

---
+++ app/src/main/java/com/example/radiotoday/ui/activities/SplashActivity.kt
@@ -0,0 +1,42 @@
+package com.example.radiotoday.ui.activities
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatDelegate
+import com.example.radiotoday.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import java.util.Calendar
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var calendar: Calendar
+    private lateinit var footer: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        val splashDelay: Long = 2000
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+
+        calendar = Calendar.getInstance()
+        val year: Int = calendar.get(Calendar.YEAR)
+
+        footer = findViewById(R.id.footer)
+        footer.text = "©Radio Today & EBS $year. All Rights Reserved"
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(splashDelay)
+
+            val intent = Intent(this@SplashActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
