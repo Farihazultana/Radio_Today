f38f65ed4a70e2623ddcd32cf0eb42bdc6ca2df2
farihazultana <farihasultana237@gmail.com>
2023-12-13 15:08:39 +0600

    ContactActivity implemented

--- app/src/main/java/com/example/radiotoday/ui/activities/ContactActivity.kt
+++ app/src/main/java/com/example/radiotoday/ui/activities/ContactActivity.kt
@@ -1,9 +1,11 @@
 package com.example.radiotoday.ui.activities
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import com.example.radiotoday.R
+import androidx.appcompat.app.AppCompatActivity
 import com.example.radiotoday.databinding.ActivityContactBinding
+
 
 class ContactActivity : AppCompatActivity() {
     private lateinit var binding: ActivityContactBinding
@@ -16,6 +18,49 @@ class ContactActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        binding.layoutContactNumber.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.setData(Uri.parse("tel:0195495301"))
+            startActivity(intent)
+        }
+
+        binding.layoutEmailAddress.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.putExtra(Intent.EXTRA_EMAIL, "help@radiotoday.com")
+            intent.type = "message/rfc822"
+            startActivity(Intent.createChooser(intent,"Choose an email client"))
+        }
+
+        binding.layoutInstagram.setOnClickListener {
+            openSocialMediaApp("com.instagram.android", "https://www.instagram.com/")
+        }
+
+        binding.layoutTwitter.setOnClickListener {
+            openSocialMediaApp("com.twitter.android", "https://twitter.com/")
+        }
+
+        binding.layoutFB.setOnClickListener {
+            openSocialMediaApp("com.facebook.katana", "https://www.facebook.com/")
+        }
+
 
     }
+
+    private fun openSocialMediaApp(packageName: String, webUrl: String) {
+        val pm = packageManager
+        try {
+            val intent = pm.getLaunchIntentForPackage(packageName)
+
+            if (intent != null) {
+                startActivity(intent)
+            } else {
+
+                val webIntent = Intent(Intent.ACTION_VIEW, Uri.parse(webUrl))
+                startActivity(webIntent)
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
+
 }
