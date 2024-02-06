e43797305008ec843b66cdf7b4a50a7feb6b6f27
farihazultana <farihasultana237@gmail.com>
2024-02-06 13:07:51 +0600

    unused files deleted

--- app/src/main/java/com/example/radiotoday/ui/fragments/SongsFragment.kt
+++ app/src/main/java/com/example/radiotoday/ui/fragments/SongsFragment.kt
@@ -8,12 +8,8 @@ import android.view.ViewGroup
 import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
-import com.bumptech.glide.request.RequestOptions.bitmapTransform
 import com.example.radiotoday.R
-import com.example.radiotoday.data.models.SubContent
 import com.example.radiotoday.databinding.FragmentSongsBinding
-import com.example.radiotoday.utils.BlurTransformation
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
